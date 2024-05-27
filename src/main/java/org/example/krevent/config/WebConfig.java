67ae8ac101655addd5caf25bbf6d45641c2bdf02
bonda29 <bondableglobe10@gmail.com>
2024-05-27 17:24:45 +0300

    cors

--- src/main/java/org/example/krevent/config/WebConfig.java
+++ src/main/java/org/example/krevent/config/WebConfig.java
@@ -2,6 +2,7 @@ package org.example.krevent.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,5 +15,15 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/qr-codes/**")
                 .addResourceLocations("file:" + qrCodePath);
+    }
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .exposedHeaders("*")
+                .allowedMethods("*")
+                .maxAge(86400);
     }
 }
